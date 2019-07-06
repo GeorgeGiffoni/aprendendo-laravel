36d1e2dd34e2dd537bd7fb24284a3ee0436bf504
George Giffoni <georgegiffoni@gmail.com>
2019-07-06 17:07:41 -0300

    implementando rotas com resourve

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,8 @@ Route::get('/idade', 'MeuControlador@getIdade');
 Route::get('/multiplicar/{n1}/{n2}', 'MeuControlador@multiplicar');
 
 Route::get('/nomes/{id}', 'MeuControlador@getNomeById');
+
+//o resource ja cria os metodos http linkados com os metodos do controlador
+Route::resource('/cliente', 'ClienteControlador');
+
+Route::post('/cliente/requisitar', 'ClienteControlador@requisitar');
