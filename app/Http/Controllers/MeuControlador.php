08e09239f15e41b77f8b9910c1b072c95dbcf51c
George Giffoni <georgegiffoni@gmail.com>
2019-07-06 16:20:24 -0300

    implementando MeuControlador

--- app/Http/Controllers/MeuControlador.php
+++ app/Http/Controllers/MeuControlador.php
@@ -8,4 +8,20 @@ class MeuControlador extends Controller{
     public function getNome(){
         return "José da Silva";
     }
+
+    public function getIdade(){
+        return "16 anos";
+    }
+
+    public function multiplicar($n1, $n2){
+        return $n1 * $n2;
+    }
+
+    public function getNomeById($id){
+        $v = ["Mario", "Edison", "Roberto", "Jean"];
+        if ( $id >= 0 && $id < count($v) ){
+            return $v[$id];
+        }
+        return "Não encontrado";
+    }
 }
