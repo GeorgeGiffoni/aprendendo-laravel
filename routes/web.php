b9dd4b8517ecca65fe2c0dc175bd727c26f01365
George Giffoni <georgegiffoni@gmail.com>
2019-07-06 18:38:06 -0300

    implementando rotas retornando view criada por mim e utilizando um metodo compact do php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/nome', 'MeuControlador@getNome');
+Route::get('/nome1231', 'MeuControlador@getNome');
 
 Route::get('/idade', 'MeuControlador@getIdade');
 
@@ -15,3 +15,26 @@ Route::get('/nomes/{id}', 'MeuControlador@getNomeById');
 Route::resource('/cliente', 'ClienteControlador');
 
 Route::post('/cliente/requisitar', 'ClienteControlador@requisitar');
+
+//seção 6 views daqui pra baixo
+
+Route::get('/primeiraview', function () {
+    return view('minhaview');
+});
+
+Route::get('/ola/{nome}/{sobrenome}', function ($nome, $sobrenome){
+    return view('minhaview')
+    ->with('nome', $nome)
+    ->with('sobrenome', $sobrenome);
+});
+
+//usando array de parametro
+Route::get('ola/user/{nome}/{sobrenome}', function($nome, $sobrenome){
+    $parametros = ['nome'=>$nome, 'sobrenome'=>$sobrenome];
+    return view('minhaview', $parametros);
+});
+
+//a mais simples de se fazer usando a funcao compact q ele vai pegar todos os parametros e montar um array associativo pra nóis
+Route::get('ola/compact/{nome}/{sobrenome}', function ($nome, $sobrenome){
+    return view('minhaview', compact('nome', 'sobrenome'));
+});
