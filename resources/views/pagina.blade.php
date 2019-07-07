f73c1d09c1fe0ba80e160933fd0e2ad44bf25a44
George Giffoni <georgegiffoni@gmail.com>
2019-07-07 11:49:19 -0300

    adicionando um alerta e passando parametros para os componentes

--- resources/views/pagina.blade.php
+++ resources/views/pagina.blade.php
@@ -5,10 +5,21 @@
     <!-- segunda maneira de incluir -->
     <link href="{{ URL::to('css/app.css')}}" rel="stylesheet">
 </head>
+
     <body>
-    <div class="alert alert-primary" role="alert">
-  A simple primary alert—check it out!
-</div>
-    <script src="{{ asset('js/app.js')}}" type="text/javascript"></script>
+    
+    <!-- PASSANDO ALGO PRO MEU COMPONENT -->
+    <!-- TUDO DENTRO DO COMPONENTE O BLADE NOMEIA COMO SENDO UM SLOT -->
+    <!-- LA DENTRO DO MEU COMPONENTE EU CHAMO  -->
+    <!-- E TUDO QUE VC COLOCA DENTRO DO COMPONENTE NA PAGINA VAI PARA SEU COMPONENTE -->
+    <!-- ATRAVES DA VARIAVEL SLOT -->
+    @component('components.meucomponente', ['tipo'=>'danger', 'titulo'=>'Deu erro'])
+        <strong>Erro: </strong> Sua mensagem de erro.
+    @endcomponent
+
     </body>
+
+    <footer>
+        <script src="{{ asset('js/app.js')}}" type="text/javascript"></script>
+    </footer>
 </html>
